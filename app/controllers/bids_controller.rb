09b9c09bc864dbc506152097d596f1dd6f724e1d
Tiffany Qi <tiffany19qi@berkeley.edu>
2016-05-19 09:39:27 -0700

    averaging almost works again

--- app/controllers/bids_controller.rb
+++ app/controllers/bids_controller.rb
@@ -50,7 +50,7 @@ class BidsController < ApplicationController
     if Listing.find(@listing).price > @bid.bid_price
       render :new, notice: 'Please create a bid price higher than the asking price.'
     else
-      @listing = Listing.find(params[:listing])
+      @listing = Listing.find(@listing)
       @listing.bid_quantity += 1
       @listing.save
       update_price(@listing, @bid, 'add')
@@ -71,26 +71,6 @@ class BidsController < ApplicationController
     end
   end
 
-  def update_price(listing, bid, action)
-    if listing.bid_quantity == 0
-      listing.average_price = listing.price
-    elsif listing.bid_quantity = 1
-      listing.average_price = bid.bid_price
-    else
-      case action
-      when 'add'
-        listing.average_price*=(listing.bid_quantity-1)
-        listing.average_price+=bid.bid_price
-        listing.average_price/=listing.bid_quantity
-      when 'remove'
-        listing.average_price*=(listng.bid_quantity+1)
-        listing.average_price-=bid.bid_price
-        listing.average_price/=listing.bid_quantity
-      end
-    end
-    listing.save
-  end
-
   # remove the bid
   def destroy
     if @bid.user_id == current_user.id
@@ -109,6 +89,27 @@ class BidsController < ApplicationController
         format.json { head :no_content }
       end
     end      
+  end
+
+
+  def update_price(listing, bid, action)
+    if listing.bid_quantity == 0
+      listing.average_price == listing.price
+    elsif listing.bid_quantity == 1
+      listing.average_price = bid.bid_price
+    else
+      case action
+      when 'add'
+        listing.average_price*=(listing.bid_quantity-1)
+        listing.average_price+=bid.bid_price
+        listing.average_price/=listing.bid_quantity
+      when 'remove'
+        listing.average_price*=(listing.bid_quantity+1)
+        listing.average_price-=bid.bid_price
+        listing.average_price/=listing.bid_quantity
+      end
+    end
+    listing.save
   end
 
   private
